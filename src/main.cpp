db8540f2471f924b4b33d464161123bb04124f8e
Jhon Ore Sulca <jefe.iot@navitrack.com.pe>
2026-06-16 16:50:28 -0500

    version1

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #define FIRMWARE_URL "https://github.com/jhonoresulca/CI_CD_ESSP32_vascode/releases/latest/download/firmware.bin"
 
 // Versión actual del firmware — cambiar en cada release
-String VERSION_LOCAL = "1.0.0";
+String VERSION_LOCAL = "1.0.1";
 
 unsigned long lastCheck = 0;
 
@@ -126,11 +126,11 @@ void loop() {
     // Parpadeo LED para confirmar que el firmware está corriendo
     digitalWrite(2, HIGH);
     Serial.println("LED ON");
-    delay(100);
+    delay(2000);
 
     digitalWrite(2, LOW);
     Serial.println("LED OFF");
-    delay(1000);
+    delay(2000);
 
     // Verificar actualización cada 10 segundos
     if (millis() - lastCheck > 10000) {
